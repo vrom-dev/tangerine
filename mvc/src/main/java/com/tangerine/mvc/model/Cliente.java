026c947b7960f6f9f4e1e13ed25c5780c72664bf
Alberto <alberto.sagi@gmail.com>
2021-05-12 09:52:35 +0200

    JavaDoc

--- mvc/src/main/java/com/tangerine/mvc/model/Cliente.java
+++ mvc/src/main/java/com/tangerine/mvc/model/Cliente.java
@@ -1,9 +1,15 @@
 package com.tangerine.mvc.model;
+/**
+ * @author Alberto (Tangerine)
+ */
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * Creacion de una clase llamada Cliente, con los siguientes atributos: id, nombre, descripcio y logo.
+ */
 public class Cliente {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -12,9 +18,19 @@ public class Cliente {
     private String descripcion;
     private String logo;
 
+    /**
+     * Constructor vacio
+     */
     public Cliente() {
     }
 
+    /**
+     * Constructor con los siguientes parametros:
+     * @param idcliente
+     * @param nombre
+     * @param descripcion
+     * @param logo
+     */
     public Cliente(Integer idcliente, String nombre, String descripcion, String logo) {
         this.id = idcliente;
         this.nombre = nombre;
@@ -22,6 +38,9 @@ public class Cliente {
         this.logo = logo;
     }
 
+    /**
+     * Getters y Setters
+     */
     public Integer getId() {
         return id;
     }
@@ -54,6 +73,9 @@ public class Cliente {
         this.logo = logo;
     }
 
+    /**
+     * Metodo toString que devuelve un String con todos los atributos de la clase.
+     */
     @Override
     public String toString() {
         return "Cliente{" +
