73e7c29878175c63be516fd717539b80c236c1f8
Alvaro-C <alvarocanasboal@gmail.com>
2021-05-18 13:49:57 +0200

    responder mensajes y checkbox

--- mvc/src/main/java/com/tangerine/mvc/service/MensajeService.java
+++ mvc/src/main/java/com/tangerine/mvc/service/MensajeService.java
@@ -1,6 +1,7 @@
 package com.tangerine.mvc.service;
 
 import com.tangerine.mvc.model.Mensaje;
+import com.tangerine.mvc.model.Proyecto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,18 +12,35 @@ import java.util.List;
 public class MensajeService {
 
     public List<Mensaje> getMensajes() {
-
         RestTemplate restTemplate = new RestTemplate();
         Mensaje[] mensajesArray = restTemplate.getForObject("http://localhost:5000/api/mensajes", Mensaje[].class);
 
         return Arrays.asList(mensajesArray);
     }
 
-    public Mensaje addMensaje(Mensaje mensajeToAdd) {
+    public Mensaje getMensajeById(Integer id) {
+        RestTemplate restTemplate = new RestTemplate();
+        Mensaje mensaje = restTemplate.getForObject("http://localhost:5000/api/mensajes/" + id, Mensaje.class, id);
 
+        return mensaje;
+    }
+
+    public Mensaje addMensaje(Mensaje mensajeToAdd) {
         RestTemplate restTemplate = new RestTemplate();
         Mensaje mensaje = restTemplate.postForObject("http://localhost:5000/api/mensaje/", mensajeToAdd, Mensaje.class );
 
         return mensaje;
+    }
+
+    public Mensaje updateMensaje(Mensaje updatedMensaje) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.put("http://localhost:5000/api/mensajes/edit", updatedMensaje);
+        return updatedMensaje;
+    }
+
+    public void borrarMensaje(Integer id) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.delete("http://localhost:5000/api/mensajes/" + id);
     }
 }
--- restapi/src/main/java/com/tangerine/restapi/service/MensajeService.java
+++ restapi/src/main/java/com/tangerine/restapi/service/MensajeService.java
@@ -1,6 +1,7 @@
 package com.tangerine.restapi.service;
 
 import com.tangerine.restapi.model.Mensaje;
+import com.tangerine.restapi.model.Proyecto;
 import com.tangerine.restapi.repository.MensajeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +29,28 @@ public class MensajeService {
     public Mensaje saveMensaje(Mensaje mensaje) {
 
         return mensajeRepository.save(mensaje);
+    }
+
+    public Mensaje editMensaje(Mensaje newMensaje) {
+
+        Integer id = newMensaje.getIdmensaje();
+        if (mensajeRepository.findById(id).isPresent()) {
+            Mensaje mensaje = new Mensaje();
+            mensaje.setIdmensaje(newMensaje.getIdmensaje());
+            mensaje.setRemitente(newMensaje.getRemitente());
+            mensaje.setFecha(newMensaje.getFecha());
+            mensaje.setSubject(newMensaje.getSubject());
+            mensaje.setMensaje(newMensaje.getMensaje());
+            mensaje.setRespuesta(newMensaje.getRespuesta());
+            mensajeRepository.save(mensaje);
+            return mensaje;
+        }
+        return newMensaje;
+
+
+    }
+
+    public void deleteMensaje(Integer id) {
+        mensajeRepository.deleteById(id);
     }
 }
